ae65d1eaf1439a24f4ef1d8e34ae9e4caa79a322
Julien Brandin <julien.brandin@supinternet.fr>
2017-10-01 19:39:00 +0200

    add task and delete task

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,32 @@
                 { title: "Tache 2 " , isDone: true },
                 { title: "Tache 3" , isDone: false },
             ],
+            task: ""
         },
 
         filters: {
             pluralize: function (word, nb){
                 return nb > 1 ? word + "s" : word;
+            }
+        },
+
+        methods:{
+            updateIsDone: function(taskNumber){
+                this.tasks[taskNumber] = false;
+            },
+
+            addTask: function (str) {
+
+                this.tasks.push(
+                    {
+                        title: str,
+                        isDone: false
+                    }
+                );
+            },
+
+            deleteTask: function (index) {
+                this.tasks.splice(index,1);
             }
         },
 
