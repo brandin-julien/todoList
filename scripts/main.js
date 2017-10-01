e14c7dbd908c9d93b1ec86f6a733363b3c205bd8
Julien Brandin <julien.brandin@supinternet.fr>
2017-10-01 19:36:19 +0200

    display number of task

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,18 @@
                 { title: "Tache 2 " , isDone: true },
                 { title: "Tache 3" , isDone: false },
             ],
+        },
+
+        filters: {
+            pluralize: function (word, nb){
+                return nb > 1 ? word + "s" : word;
+            }
+        },
+
+        computed: {
+            remainingComputed: function () {
+                return this.tasks.filter(task => task.isDone).length;
+            }
         }
 
     });
